50375510d5e5d5565ce7da6fbca51b67337c8b8e
deeaanghelache <andreea.anghelache@s.unibuc.ro>
2024-01-11 15:16:54 +0200

    Added Api response and Operation for user Controller

--- AppBackend/src/main/java/com/unibuc/appbackend/controllers/UserController.java
+++ AppBackend/src/main/java/com/unibuc/appbackend/controllers/UserController.java
@@ -2,6 +2,10 @@ package com.unibuc.appbackend.controllers;
 
 import com.unibuc.appbackend.entities.User;
 import com.unibuc.appbackend.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,34 +22,62 @@ public class UserController {
     }
 
     @PostMapping("/register")
-//    pune api operation pt swagger
-    public ResponseEntity<User> create(@RequestBody User user) {
+    @Operation(summary = "Create an user", description = "Creates a new user based on the information received in the request's body")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "The user has been successfully created!"),
+            @ApiResponse(responseCode = "409", description = "There is already an user with the specified email!")
+    })
+    public ResponseEntity<User> create(@RequestBody @Parameter(description = "User data provided by the register form") User user) {
         return ResponseEntity.ok(userService.create(user));
     }
 
     @PostMapping("/login")
-    public ResponseEntity<User> login(@RequestBody User user) {
+    @Operation(summary = "Login an user", description = "Checks if the password and email provided in the request's body are correct")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "The user has been successfully logged in!"),
+            @ApiResponse(responseCode = "401", description = "Email or password incorrect!")
+    })
+    public ResponseEntity<User> login(@RequestBody @Parameter(description = "User data provided by the login form") User user) {
         return ResponseEntity.ok(userService.login(user));
     }
 
+    @Operation(summary = "Get information about an user", description = "Get information about a certain user by providing their id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "User was found in the database"),
+            @ApiResponse(responseCode = "404", description = "User was NOT found in the database")
+    })
     @GetMapping("/getById/{id}")
-    public ResponseEntity<User> getUserById(@PathVariable("id") String id) {
+    public ResponseEntity<User> getUserById(@PathVariable("id") @Parameter(description = "The uuid of the user you want to get information about") String id) {
         return ResponseEntity.ok(userService.getUserById(id));
     }
 
+    @Operation(summary = "Get all users", description = "Returns all users in the database")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "A list containing all the users in the database")
+    })
     @GetMapping("/getAll")
     public ResponseEntity<List<User>> getAllUsers() {
         return ResponseEntity.ok(userService.getAllUsers());
     }
 
+    @Operation(summary = "Change password for given user", description = "Change the password of a certain user by providing their id and new password")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "User was found in the database"),
+            @ApiResponse(responseCode = "404", description = "User was NOT found in the database")
+    })
     @PostMapping("/changePassword/{id}")
-    public ResponseEntity<?> changePassword(@PathVariable("id") String id, @RequestBody String password){
+    public ResponseEntity<?> changePassword(@PathVariable("id") @Parameter(description = "The uuid of the user") String id, @RequestBody String password){
         userService.changePassword(id, password);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    @Operation(summary = "Delete a given user", description = "Delete a certain user by providing their id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "User was found in the database"),
+            @ApiResponse(responseCode = "404", description = "User was NOT found in the database")
+    })
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<?> delete(@PathVariable("id") String id){
+    public ResponseEntity<?> delete(@PathVariable("id") @Parameter(description = "The uuid of the user") String id){
         userService.delete(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
