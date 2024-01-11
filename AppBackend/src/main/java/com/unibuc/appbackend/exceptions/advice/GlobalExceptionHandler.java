c5f2018c109e3a3468b7c03787c6f5f86f35b3fb
deeaanghelache <andreea.anghelache@s.unibuc.ro>
2024-01-11 19:17:29 +0200

    Global Exception Handler

---
+++ AppBackend/src/main/java/com/unibuc/appbackend/exceptions/advice/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.unibuc.appbackend.exceptions.advice;
+
+import com.unibuc.appbackend.exceptions.ProjectNotFoundException;
+import com.unibuc.appbackend.exceptions.SprintNotFoundException;
+import com.unibuc.appbackend.exceptions.UserAlreadyExistsException;
+import com.unibuc.appbackend.exceptions.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler({UserAlreadyExistsException.class})
+    public ResponseEntity<?> handle(UserAlreadyExistsException userAlreadyExistsException) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(userAlreadyExistsException.getMessage());
+    }
+
+    @ExceptionHandler({UserNotFoundException.class})
+    public ResponseEntity<?> handle(UserNotFoundException userNotFoundException) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(userNotFoundException.getMessage());
+    }
+
+    @ExceptionHandler({ProjectNotFoundException.class})
+    public ResponseEntity<?> handle(ProjectNotFoundException projectNotFoundException) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(projectNotFoundException.getMessage());
+    }
+
+    @ExceptionHandler({SprintNotFoundException.class})
+    public ResponseEntity<?> handle(SprintNotFoundException sprintNotFoundException) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(sprintNotFoundException.getMessage());
+    }
+}
