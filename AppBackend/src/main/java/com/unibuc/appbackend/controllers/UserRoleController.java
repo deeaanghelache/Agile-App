2f54a925911a32d2bd3c7f132f78ab97cf9d2286
deeaanghelache <andreea.anghelache@s.unibuc.ro>
2024-01-17 12:19:07 +0200

    Check Scrum Master and Admin role functions

--- AppBackend/src/main/java/com/unibuc/appbackend/controllers/UserRoleController.java
+++ AppBackend/src/main/java/com/unibuc/appbackend/controllers/UserRoleController.java
@@ -1,9 +1,15 @@
 package com.unibuc.appbackend.controllers;
 
 import com.unibuc.appbackend.services.UserRoleService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController()
 @RequestMapping("/userRole")
@@ -14,5 +20,25 @@ public class UserRoleController {
 
     public UserRoleController(UserRoleService userRoleService) {
         this.userRoleService = userRoleService;
+    }
+
+    @Operation(summary = "Check if a user is admin", description = "Check if a user is admin by providing their id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "User was found in the database"),
+            @ApiResponse(responseCode = "404", description = "User was NOT found in the database")
+    })
+    @GetMapping("/checkAdmin/{id}")
+    public ResponseEntity<Boolean> checkIfUserIsAdmin(@PathVariable("id") @Parameter(description = "The id of the user you want to check") UUID userId) {
+        return ResponseEntity.ok(userRoleService.checkAdminRoleForGivenUser(userId));
+    }
+
+    @Operation(summary = "Check if a user is scrum master", description = "Check if a user is scrum master by providing their id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "User was found in the database"),
+            @ApiResponse(responseCode = "404", description = "User was NOT found in the database")
+    })
+    @GetMapping("/checkScrumMaster/{id}")
+    public ResponseEntity<Boolean> checkIfUserIsScrumMaster(@PathVariable("id") @Parameter(description = "The id of the user you want to check") UUID userId) {
+        return ResponseEntity.ok(userRoleService.checkScrumMasterRoleForGivenUser(userId));
     }
 }
--- AppBackend/src/main/java/com/unibuc/appbackend/services/UserRoleService.java
+++ AppBackend/src/main/java/com/unibuc/appbackend/services/UserRoleService.java
@@ -8,15 +8,20 @@ import com.unibuc.appbackend.enums.RoleName;
 import com.unibuc.appbackend.interfaces.UserRoleRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.UUID;
+
 @Service
 public class UserRoleService {
 
     private UserRoleRepository userRoleRepository;
     private RoleService roleService;
+    private UserService userService;
 
-    public UserRoleService(UserRoleRepository userRoleRepository, RoleService roleService) {
+    public UserRoleService(UserRoleRepository userRoleRepository, RoleService roleService, UserService userService) {
         this.userRoleRepository = userRoleRepository;
         this.roleService = roleService;
+        this.userService = userService;
     }
 
     public void addRoleForUser(User user) {
@@ -25,5 +30,32 @@ public class UserRoleService {
 
         UserRole userRole = new UserRole(userRoleEmbeddedId, user, role);
         userRoleRepository.save(userRole);
+    }
+
+    public List<UserRole> getAllRolesForGivenUser(UUID userId){
+        var currentUser = userService.getUserById(userId);
+        return userRoleRepository.queryBy(currentUser.getUserId());
+    }
+
+    public Boolean checkAdminRoleForGivenUser(UUID userId) {
+        var userRoles = getAllRolesForGivenUser(userId);
+
+        for (var userRole : userRoles) {
+            if (userRole.getRole().getRoleName().equals(RoleName.ADMIN)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Boolean checkScrumMasterRoleForGivenUser(UUID userId) {
+        var userRoles = getAllRolesForGivenUser(userId);
+
+        for (var userRole : userRoles) {
+            if (userRole.getRole().getRoleName().equals(RoleName.SCRUM_MASTER)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
